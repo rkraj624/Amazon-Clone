0163bbbd57a1d9c6905693d43462ee515707f0d2
Ravi Kishan Raj <ravikishan624@.com>
2022-07-30 14:31:25 +0530

    header completed

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className="home_container">
+            <img className='home_image' src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Electronics/GW/Unrec/Heros/under_1999_Tallhero_3000x1200._CB634376407_.jpg" alt="" />
+        </div>
+        <div className="home_row">
+            {/* Product */}
+            {/* Product */}
+        </div>
+        <div className="home_row">
+            {/* Product */}
+            {/* Product */}
+            {/* Product */}
+        </div>
+        <div className="home_row">
+            {/* Product */}
+        </div>
+    </div>
+  )
+}
+
+export default Home
